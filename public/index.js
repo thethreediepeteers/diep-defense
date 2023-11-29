e3b7b4d8270ee5c93f8a67935e1be17ed726b18a
Hussain Abidi <hussainabidi2009@gmail.com>
2023-11-29 20:09:06 +0500

    quadtree

--- public/index.js
+++ public/index.js
@@ -1,1 +1,12 @@
-const socket = new WebSocket("ws://localhost:3001/");
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const initSocket = () => {
+    const socket = new WebSocket("ws://localhost:3001/");
+    return socket;
+}
+
+initSocket();
